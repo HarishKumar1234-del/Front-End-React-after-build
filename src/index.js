d2e4b6958899b44ea11f20ab49a78f2f25fc6f14
harish kumar <dalchand4ud@gmail.com>
2020-06-01 19:32:04 +0530

    header and footer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+
 // boostrap first my own css later because index.css helps to override bootstrap
 import './index.css';
 import App from './App';
